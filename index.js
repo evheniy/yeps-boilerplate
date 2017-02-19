545ebffe4d74f770d07f5098fbcd37a2f760bbac
Evheniy Bystrov <evheniy.bystrov@just-eat.com>
2017-02-19 17:30:00 +0000

    test calls

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const Router = require('yeps-router');
 const redis = require('yeps-redis');
 const error = require('yeps-error');
 const logger = require('yeps-logger');
+const parser = require('url');
+const config = require('config');
+require('isomorphic-fetch');
 
 const app = module.exports = new App();
 const router = new Router();
@@ -13,9 +16,46 @@ app.all([
     logger(),
 ]);
 
-router.get('/').then(async ctx => {
+router.get('/users').then(async ctx => {
     ctx.res.writeHead(200, {'Content-Type': 'application/json'});
-    ctx.res.end(JSON.stringify({ test: 1 }));
+    ctx.res.end(JSON.stringify([
+        { id: 1, name: 'Tom' },
+        { id: 2, name: 'Nick' },
+        { id: 3, name: 'Phil' },
+        { id: 4, name: 'Vlad' },
+        { id: 5, name: 'Ev' },
+    ]));
+}).get('/categories').then(async ctx => {
+    ctx.res.writeHead(200, {'Content-Type': 'application/json'});
+    ctx.res.end(JSON.stringify([
+        { id: 1, name: 'Friends' },
+        { id: 2, name: 'Office' },
+        { id: 3, name: 'Family' },
+    ]));
+}).get('/').then(async ctx => {
+    let response = {};
+
+    let data = await ctx.redis.get(ctx.req.url);
+
+    if (data) {
+        response = JSON.parse(data);
+    } else {
+        const query = parser.parse(ctx.req.url, true).query;
+
+        data = await Promise.all(
+            Object.keys(query).map(key => fetch(`http://${ctx.req.headers.host}/${query[key]}`).then(res => res.json()))
+        );
+
+        Object.keys(query).forEach((key, index) => {
+            response[key] = data[index];
+        });
+
+        await ctx.redis.set(ctx.req.url, JSON.stringify(response), 'ex', config.ttl);
+    }
+
+    ctx.res.writeHead(200, {'Content-Type': 'application/json'});
+    ctx.res.end(JSON.stringify(response));
+
 });
 
 app.then(router.resolve());
--- tests/index.js
+++ tests/index.js
@@ -3,18 +3,12 @@ const http = require('http');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
+const config = require('config');
+const Redis = require('ioredis');
+const redis = new Redis(config.redis);
 
 chai.use(chaiHttp);
 let server;
-
-/* app.then(async ctx => {
-    sinon.stub(ctx.logger, 'info', text => text);
-    sinon.stub(ctx.logger, 'error', text => text);
-});
-
-app.then(async ctx => {
-    ctx.logger.restore();
-}); */
 
 describe('YEPS app boilerplate test', () => {
 
@@ -22,17 +16,186 @@ describe('YEPS app boilerplate test', () => {
         server = http.createServer(app.resolve());
     });
 
-    it('should test', async () => {
+    it('should test users', async () => {
         let isTestFinished = false;
 
         await chai.request(server)
-            .get('/')
+            .get('/users')
             .send()
             .then(res => {
                 expect(res).to.have.status(200);
+                expect(res.body).is.an('array');
+                expect(res.body.length).to.be.equal(5);
+                expect(res.body[0]).is.an('object');
+                expect(res.body[0]).to.have.property('id');
+                expect(res.body[0]).to.have.property('name');
+                expect(res.body[0].id).is.not.empty;
+                expect(res.body[0].name).is.not.empty;
+                expect(res.body[0].id).to.be.equal(1);
+                expect(res.body[0].name).to.be.equal('Tom');
                 isTestFinished = true;
             });
 
         expect(isTestFinished).is.true;
+    });
+
+    it('should test categories', async () => {
+        let isTestFinished = false;
+
+        await chai.request(server)
+            .get('/categories')
+            .send()
+            .then(res => {
+                expect(res).to.have.status(200);
+                expect(res.body).is.an('array');
+                expect(res.body.length).to.be.equal(3);
+                expect(res.body[0]).is.an('object');
+                expect(res.body[0]).to.have.property('id');
+                expect(res.body[0]).to.have.property('name');
+                expect(res.body[0].id).is.not.empty;
+                expect(res.body[0].name).is.not.empty;
+                expect(res.body[0].id).to.be.equal(1);
+                expect(res.body[0].name).to.be.equal('Friends');
+                isTestFinished = true;
+            });
+
+        expect(isTestFinished).is.true;
+    });
+
+    it('should test aggregator from redis', async () => {
+        let isTestFinished = false;
+
+        const users = [
+            { id: 1, name: 'Tom' },
+            { id: 2, name: 'Nick' },
+            { id: 3, name: 'Phil' },
+            { id: 4, name: 'Vlad' },
+            { id: 5, name: 'Ev' },
+        ];
+        const categories = [
+            { id: 1, name: 'Friends' },
+            { id: 2, name: 'Office' },
+            { id: 3, name: 'Family' },
+        ];
+
+        await redis.set('/?users=users&categories=categories', JSON.stringify({ users, categories}));
+
+        await chai.request(server)
+            .get('/')
+            .query({
+                users: 'users',
+                categories: 'categories'
+            })
+            .send()
+            .then(res => {
+                expect(res).to.have.status(200);
+                expect(res.body).is.an('object');
+                expect(res.body.users).is.not.empty;
+                expect(res.body.users).is.an('array');
+                expect(res.body.categories).is.not.empty;
+                expect(res.body.categories).is.an('array');
+
+                expect(res.body.users.length).to.be.equal(5);
+                expect(res.body.users[0]).is.an('object');
+                expect(res.body.users[0]).to.have.property('id');
+                expect(res.body.users[0]).to.have.property('name');
+                expect(res.body.users[0].id).is.not.empty;
+                expect(res.body.users[0].name).is.not.empty;
+                expect(res.body.users[0].id).to.be.equal(1);
+                expect(res.body.users[0].name).to.be.equal('Tom');
+
+                expect(res.body.categories).is.an('array');
+                expect(res.body.categories.length).to.be.equal(3);
+                expect(res.body.categories[0]).is.an('object');
+                expect(res.body.categories[0]).to.have.property('id');
+                expect(res.body.categories[0]).to.have.property('name');
+                expect(res.body.categories[0].id).is.not.empty;
+                expect(res.body.categories[0].name).is.not.empty;
+                expect(res.body.categories[0].id).to.be.equal(1);
+                expect(res.body.categories[0].name).to.be.equal('Friends');
+
+                isTestFinished = true;
+            });
+
+        await redis.del('/?users=users&categories=categories');
+
+        expect(isTestFinished).is.true;
+    });
+
+    it('should test aggregator from redis', async () => {
+        let isTestFinished = false;
+
+        await redis.del('/?users=users&categories=categories');
+
+        await chai.request(server)
+            .get('/')
+            .query({
+                users: 'users',
+                categories: 'categories'
+            })
+            .send()
+            .then(res => {
+                expect(res).to.have.status(200);
+                expect(res.body).is.an('object');
+                expect(res.body.users).is.not.empty;
+                expect(res.body.users).is.an('array');
+                expect(res.body.categories).is.not.empty;
+                expect(res.body.categories).is.an('array');
+
+                expect(res.body.users.length).to.be.equal(5);
+                expect(res.body.users[0]).is.an('object');
+                expect(res.body.users[0]).to.have.property('id');
+                expect(res.body.users[0]).to.have.property('name');
+                expect(res.body.users[0].id).is.not.empty;
+                expect(res.body.users[0].name).is.not.empty;
+                expect(res.body.users[0].id).to.be.equal(1);
+                expect(res.body.users[0].name).to.be.equal('Tom');
+
+                expect(res.body.categories).is.an('array');
+                expect(res.body.categories.length).to.be.equal(3);
+                expect(res.body.categories[0]).is.an('object');
+                expect(res.body.categories[0]).to.have.property('id');
+                expect(res.body.categories[0]).to.have.property('name');
+                expect(res.body.categories[0].id).is.not.empty;
+                expect(res.body.categories[0].name).is.not.empty;
+                expect(res.body.categories[0].id).to.be.equal(1);
+                expect(res.body.categories[0].name).to.be.equal('Friends');
+
+                isTestFinished = true;
+            });
+
+        expect(isTestFinished).is.true;
+
+        let data = await redis.get('/?users=users&categories=categories');
+
+        expect(data).is.not.empty;
+        data = JSON.parse(data);
+
+        expect(data).is.an('object');
+        expect(data.users).is.not.empty;
+        expect(data.users).is.an('array');
+        expect(data.categories).is.not.empty;
+        expect(data.categories).is.an('array');
+
+        expect(data.users.length).to.be.equal(5);
+        expect(data.users[0]).is.an('object');
+        expect(data.users[0]).to.have.property('id');
+        expect(data.users[0]).to.have.property('name');
+        expect(data.users[0].id).is.not.empty;
+        expect(data.users[0].name).is.not.empty;
+        expect(data.users[0].id).to.be.equal(1);
+        expect(data.users[0].name).to.be.equal('Tom');
+
+        expect(data.categories).is.an('array');
+        expect(data.categories.length).to.be.equal(3);
+        expect(data.categories[0]).is.an('object');
+        expect(data.categories[0]).to.have.property('id');
+        expect(data.categories[0]).to.have.property('name');
+        expect(data.categories[0].id).is.not.empty;
+        expect(data.categories[0].name).is.not.empty;
+        expect(data.categories[0].id).to.be.equal(1);
+        expect(data.categories[0].name).to.be.equal('Friends');
+
+        await redis.del('/?users=users&categories=categories');
     });
 });
